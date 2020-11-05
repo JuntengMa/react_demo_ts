6241c6af4e8e5374d1627e4741c7a6fb2617b8a8
v_vjtma <v_vjtma@tencent.com>
2020-11-05 16:22:35 +0800

    配置antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import './App.less'
+import { Button } from 'antd';
+
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Button type="primary">1234</Button>
+    </div>
   );
 }
 
