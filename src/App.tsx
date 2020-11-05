6d3fa9a8a14ffb6701e3f19939e519fdee0e102d
v_vjtma <v_vjtma@tencent.com>
2020-11-05 11:57:14 +0800

    react-demo-ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import './App.less';
-import { Test } from './pages/test/Test'
 function App() {
   return (
-    <div className="App">
-      <Test />
-    </div>
+    <div className="App"></div>
   );
 }
 
